8ae3b93d73af581e5381708d3dcbac1394fb02d0
Trevor <jaguarxman@gmail.com>
2024-12-02 19:59:02 -0500

    day 2 compelte

---
+++ day2/day2_part1.py
@@ -0,0 +1,28 @@
+with open("day2\input.txt", "r") as file:
+    data = file.read().splitlines()
+
+def safe_checker(number_list:list):
+    difference = [ int(x) - int(y) for x, y in zip(number_list, number_list[1:])]
+    pos_status = []
+    for d in difference:
+        if 1<= d <=3:
+            pos_status.append(True)
+        else:
+            pos_status.append(False)
+            break
+    neg_status = []
+    for d in difference:
+        if -1>= d >=-3:
+            neg_status.append(True)
+        else:
+            neg_status.append(False)
+            break
+    return all(pos_status) or all(neg_status)
+
+safe = 0
+for d in data:
+    numbers = d.split(" ")
+    if safe_checker(numbers):
+        safe += 1
+print(safe)
+
